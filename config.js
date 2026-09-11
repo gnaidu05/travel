9044e13bc0eece81422507058d43490018d16ee7
gnaidu05 <gnaidu05@gmail.com>
2026-09-11 13:14:55 +0530

    Connect CampusRoute to Supabase

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CAMPUS_ROUTE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://gjiqwytcvdykexblfggb.supabase.co",
+  supabaseAnonKey: "sb_publishable_FO3Ey3Jkoa1WoM0k1CEpTQ_7_HXtSLH",
   allowedDomain: "capgemini.com"
 };
